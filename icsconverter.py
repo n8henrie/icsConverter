27d1b63d79c75d9f10498d05e203779a67f972c9
n8henrie@gmail.com <n8henrie@gmail.com>
2013-04-22 20:43:06 -0600

    Major changes. Went to using csvdictreader to make the code more readable and make the header order irrelevant.

--- icsconverter.py
+++ icsconverter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+# See my post on the script here: http://n8henrie.com/
 
 # I got help from here:
-# http://www.linuxjournal.com/content/handling-csv-files-python
-# Help from here: http://unix.stackexchange.com/questions/7425/is-there-a-robust-command-line-tool-for-processing-csv-files
-# Help from here: http://stackoverflow.com/questions/9319317/quick-and-easy-file-dialog-in-python
+# http://bit.ly/Z4PoR3
+# and here: http://bit.ly/Z4Plov
+# and here: http://bit.ly/Z4PmIU
+# and here http://bit.ly/110xGfV
 
 import csv
 from icalendar import Calendar, Event, LocalTimezone
@@ -13,71 +15,128 @@ import easygui
 from sys import exit
 from os.path import expanduser,isdir
 
-try:
-	if isdir(expanduser("~/Desktop")):
-	    reader = csv.reader(open(easygui.fileopenbox(msg="Please select the .csv file to be converted to .ics", title="", default=expanduser("~/Desktop/"), filetypes=["*.csv"]), 'rb'))
-	else:
-		reader = csv.reader(open(easygui.fileopenbox(msg="Please select the .csv file to be converted to .ics", title="", default=expanduser("~/"), filetypes=["*.csv"]), 'rb'))
-except:
-    exit(0)
+def CheckHeaders(headers):
+    '''Makes sure that all the headers are exactly
+    correct so that they'll be recognized as the
+    necessary keys.'''
 
-try:
-    #Start calendar file
-    cal = Calendar()
-    cal.add('prodid', 'n8henrie.com')
-    cal.add('version', '2.0')
+    valid_keys = ['End Date', 'Description',
+    'All Day Event', 'Start Time', 'Private',
+    'End Time', 'Location', 'Start Date', 'Subject']
 
-    #Run through the CSV row by row, processing its elements to cal
-    rownum = 0;
-    for row in reader:
-        if rownum > 0 and row[0] != '':
-          event = Event()
-          event.add('summary', row[0])
+    for header in headers:
+        if header not in valid_keys:
+            easygui.msgbox('Looks like one or more of your headers is not quite right, so the script will exit after this. Make sure there aren\'t any leading or trailing spaces, and check the capitalization, and try again. The headers need to be *exactly* like this (without the quotes):\n\n{0}\n\nLooks like the first problematic header was: "{1}".'.format('"' + '" "'.join(valid_keys) + '"', header))
 
-    #If marked as an "all day event," ignore times. icalendar module doesn't like bare dates
-    #without times, so will default to midnight. Therefore add 1 day to end date to make a 24
-    #hour period. Set as transparent (= free / not busy). If start and end date are the same
-    #or if end date is blank default to a single 24-hour event.
-          if row[5].lower() == "true":
-            event.add('transp', "TRANSPARENT")
-            event.add('dtstart', datetime.strptime(row[1], "%m/%d/%Y" ))
-            if row[1] == row[3] or row[3] == '':
-              event.add('dtend', datetime.strptime(row[1], "%m/%d/%Y" ) + timedelta(days=1))
-            else:
-              event.add('dtend', datetime.strptime(row[3], "%m/%d/%Y" ) + timedelta(days=1))
+            exit(1)
+        else:
+            pass
 
-    #Continue processing events not marked as "all day event."
-          else:
-            if row[2][-2:].lower() in ['am','pm']:
-              event.add('dtstart', datetime.strptime(row[1] + row[2], "%m/%d/%Y%I:%M %p" ))
-            else:
-              event.add('dtstart', datetime.strptime(row[1] + row[2], "%m/%d/%Y%H:%M" ))
+def CleanSpaces(csv_dict):
+    '''Cleans trailing spaces from the dictionary
+    values, which can break my datetime patterns.'''
+    clean_row = {}
+    for row in csv_dict:
+        for k, v in row.items():
+            if k:
+                clean_row.update({ k: v.strip()})
+        yield clean_row
 
-    #Allow either 24 hour time or 12 hour + am/pm
-            if row[4][-2:].lower() in ['am','pm']:
-              event.add('dtend', datetime.strptime(row[3] + row[4], "%m/%d/%Y%I:%M %p" ))
-            else:
-              event.add('dtend', datetime.strptime(row[3] + row[4], "%m/%d/%Y%H:%M" ))
-
-          event.add('description', row[6])
-          event.add('location', row[7])
-          event.add('dtstamp', datetime.replace( datetime.now(), tzinfo=LocalTimezone() ))
-          event['uid'] = str(randint(1,10**30)) + datetime.now().strftime("%Y%m%dT%H%M%S") + '___n8henrie.com'
-
-          cal.add_component(event)
-        rownum += 1
-except:
-	easygui.msgbox("Looks like there was a problem parsing the .csv file.")
-	exit(0)
-
-    #Write final .ics file to same directory as input file.
+# The skipinitialspace option eliminates leading
+# spaces and reduces blank cells to '' while the CleanSpaces
+# function gets rid of trailing spaces.
 try:
     if isdir(expanduser("~/Desktop")):
-	    f = open(easygui.filesavebox(msg="Save .ics File", title="", default=expanduser("~/Desktop/") + "calendar.ics", filetypes=["*.ics"]), 'wb')
+        reader_builder = list(csv.DictReader(open(easygui.fileopenbox(msg="Please select the .csv file to be converted to .ics", title="", default=expanduser("~/Desktop/"), filetypes=["*.csv"]), 'rb')))
     else:
-	    f = open(easygui.filesavebox(msg="Save .ics File", title="", default=expanduser("~/") + "calendar.ics", filetypes=["*.ics"]), 'wb')
+        reader_builder = list(csv.DictReader(open(easygui.fileopenbox(msg="Please select the .csv file to be converted to .ics", title="", default=expanduser("~/"), filetypes=["*.csv"]), 'rb')))
+except:
+    easygui.msgbox('Looks like there was an error opening the file, didn\'t even make it to the conversion part. Sorry!')
+    exit(1)
+
+# Filter out events with empty subjects, a required element
+# for a calendar event.
+# Code found here: http://bit.ly/Z4Pg4h
+reader_builder[:] = [d for d in reader_builder if d.get('Subject') != '']
+
+headers = reader_builder[0].keys()
+CheckHeaders(headers)
+
+reader = CleanSpaces(reader_builder)
+
+# Start calendar file
+cal = Calendar()
+cal.add('prodid', 'n8henrie.com')
+cal.add('version', '2.0')
+
+# Write the clean list of dictionaries to events.
+try:
+    rownum = 0
+    for row in reader:
+        event = Event()
+        event.add('summary', row['Subject'])
+
+    # If marked as an "all day event," ignore times.
+    # icalendar module doesn't like bare dates
+    # without times, so will default to midnight.
+    # Therefore add 1 day to end date to make a 24
+    # hour period. Set as transparent (= free / not busy).
+    # If start and end date are the same
+    # or if end date is blank default to a single 24-hour event.
+        if row['All Day Event'].lower() == 'true':
+            event.add('transp', 'TRANSPARENT')
+            event.add('dtstart', datetime.strptime(row['Start Date'], '%m/%d/%Y' ))
+    #            pdb.set_trace()
+            if row['Start Date'] == row['End Date'] or row['End Date'] == '':
+                event.add('dtend', datetime.strptime(row['Start Date'], '%m/%d/%Y' ) + timedelta(days=1))
+            else:
+                event.add('dtend', datetime.strptime(row['End Date'], '%m/%d/%Y' ) + timedelta(days=1))
+
+    # Continue processing events not marked as "all day" events.
+        else:
+
+    # Allow either 24 hour time or 12 hour + am/pm
+            if row['Start Time'][-2:].lower() in ['am','pm']:
+                event.add('dtstart', datetime.strptime(row['Start Date'] + row['Start Time'], '%m/%d/%Y%I:%M %p' ))
+            else:
+                event.add('dtstart', datetime.strptime(row['Start Date'] + row['Start Time'], '%m/%d/%Y%H:%M' ))
+
+    # Allow blank end dates (assume same day)
+            if row['End Date'] == '':
+                row['End Date'] = row['Start Date']
+
+            if row['End Time'][-2:].lower() in ['am','pm']:
+                event.add('dtend', datetime.strptime(row['End Date'] + row['End Time'], '%m/%d/%Y%I:%M %p' ))
+            else:
+                event.add('dtend', datetime.strptime(row['End Date'] + row['End Time'], '%m/%d/%Y%H:%M' ))
+
+        event.add('description', row['Description'])
+        event.add('location', row['Location'])
+        event.add('dtstamp', datetime.replace( datetime.now(), tzinfo=LocalTimezone() ))
+        event['uid'] = str(randint(1,10**30)) + datetime.now().strftime('%Y%m%dT%H%M%S') + '___n8henrie.com'
+
+        cal.add_component(event)
+    rownum += 1
+
+except:
+    if rownum > 0:
+        easygui.msgbox('I had a problem with an event. I think I might have gotten through about {0} events and had trouble with an event with subject: {1}. Sorry!'.format(rownum, row['Subject']))
+    elif rownum == 0:
+        easygui.msgbox('Looks like I didn\'t even get through the first event. Sorry!')
+    else:
+        easygui.msgbox('Somehow it looks like I processed negative events... that shouldn\'t have happened. Sorry!')
+    exit(2)
+
+try:
+    # Write final .ics file to same directory as input file.
+    if isdir(expanduser('~/Desktop')):
+        f = open(easygui.filesavebox(msg='Save .ics File', title='', default=expanduser('~/Desktop/') + 'calendar.ics', filetypes=['*.ics']), 'wb')
+    else:
+        f = open(easygui.filesavebox(msg='Save .ics File', title='', default=expanduser('~/') + 'calendar.ics', filetypes=['*.ics']), 'wb')
 
     f.write(cal.to_ical())
     f.close()
+
 except:
-    exit(0)
+    easygui.msgbox('Looks like the conversion went okay, but there was some kind of error writing the file. Sorry!')
+    exit(3)
